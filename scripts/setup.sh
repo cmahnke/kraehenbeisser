b697f73e01ee87f2c2de3317421bed900d391ee4
Christian Mahnke <cmahnke@gmail.com>
2021-05-16 21:24:11 +0200

    Added python setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,7 +13,7 @@
 # IIIF tiles
 echo "Set SKIP_IIIF to something to disable generation of IIIF derivates"
 
-if [[ -z "$SKIP_IIIF" ]] ; then
+if [ -z "$SKIP_IIIF" ] ; then
     ./scripts/iiif.sh
 fi
 
